7e6dba87d016cb5d83f60601ef3721907147ebd8
lotfi80 <salioslim@hotmail.de>
2024-11-27 14:46:35 +0100

    fixing the issue with socket.io

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -14,7 +14,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5001", {
+      const socket = io("https://chat-app-production-zyr2.onrender.com", {
         query: {
           userId: authUser._id,
         },
